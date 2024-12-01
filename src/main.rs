96410b71a7791627b65e99548fa7bc0115b3f24e
Pharaz Fadaei <pharaz.apps@gmail.com>
2024-12-01 20:44:07 +0330

    Implemented exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,72 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::Chars;
+
+struct LineTokenIter<'a> {
+    chars: Chars<'a>,
+}
+
+impl<'a> LineTokenIter<'a> {
+    pub fn new(line: &'a str) -> Self {
+        LineTokenIter {
+            chars: line.chars(),
+        }
+    }
+}
+
+impl<'a> Iterator for LineTokenIter<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut token = String::new();
+        let mut in_quotes = false;
+
+        while let Some(ch) = self.chars.next() {
+            match ch {
+                '"' => in_quotes = !in_quotes,
+                '\\' => match self.chars.next() {
+                    Some(next) => token.push(next),
+                    None => panic!("Line ended in a '\\'."),
+                },
+                ' ' if !in_quotes && token.len() != 0 => break,
+                _ => token.push(ch),
+            }
+        }
+
+        Some(token)
+    }
+}
+
+enum Command {
+    Exit(i32),
+    NotFound,
+}
+
+impl Command {
+    pub fn parse(line: &str) -> anyhow::Result<Command> {
+        let mut tokens = LineTokenIter::new(line);
+
+        let name = match tokens.next() {
+            Some(token) => token,
+            None => anyhow::bail!("Line is empty"),
+        };
+
+        Ok(match name.as_ref() {
+            "exit" => {
+                let rest: Vec<_> = tokens.collect();
+
+                let code = match rest.len() {
+                    0 => 127,
+                    1 => rest[0].parse()?,
+                    _ => anyhow::bail!("Too many arguments (expected 2"),
+                };
+
+                Command::Exit(code)
+            }
+            _ => Command::NotFound,
+        })
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -11,6 +78,22 @@ fn main() {
         // Wait for user input
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        let command = match Command::parse(&input) {
+            Ok(cmd) => cmd,
+            Err(err) => {
+                println!("{:?}", err);
+                continue;
+            }
+        };
+
+        match command {
+            Command::Exit(code) => {
+                std::process::exit(code);
+            }
+            Command::NotFound => {
+                println!("{}: command not found", input.trim());
+            }
+        }
     }
 }
